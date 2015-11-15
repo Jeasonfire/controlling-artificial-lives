ae4f0d18fafefa1954b79879e756b272bf3b0111
Jeasonfire <jeasonfire@gmail.com>
2015-11-15 04:09:04 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,17 @@
+Controlling Artificial Lives
+============================
+
+Table of Contents
+-----------------
+* [General](#general)
+* [License](#license)
+
+General
+-------
+Controlling Artificial Lives is a text-based simulation game made in JavaScript.
+
+License
+-------
+The code in this repository is licensed under the [GNU GPL 3.0](https://www.gnu.org/licenses/gpl-3.0) license. See the LICENSE.txt file for more details.
+
+For a quick reference as to what this means, see [the TLDRLegal page about GNU GPL 3.0](https://tldrlegal.com/license/gnu-general-public-license-v3-%28gpl-3%29) - **Note**: this is not legal advice and so on, as the site itself says.
---
+++ js/main.js
@@ -0,0 +1,78 @@
+/*
+ *  Copyright (C) 2015  Jeasonfire
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+var TIME_PASSED_PER_SECOND = 0.2;
+var people = [];
+var resources = {
+    food: 0,
+    build: 0
+}
+var historyEntries = [];
+
+function makePerson() {
+    var person = new Person();
+    people.push(person);
+    historyEntries.push(person.name + " was just born!");
+}
+
+function gatherFood() {
+    resources.food++;
+}
+
+function gatherBuild() {
+    resources.build++;
+}
+
+function update(delta) {
+    for (var i = 0; i < people.length; i++) {
+        var lastAge = people[i].age;
+        people[i].age += delta * TIME_PASSED_PER_SECOND;
+        if (Math.floor(lastAge / 10.0) != Math.floor(people[i].age / 10.0)) {
+            historyEntries.push(people[i].name + " just turned " + Math.round(people[i].age) + "!");
+        }
+    }
+}
+
+/* The game loop code (a very hacky loop system that works on its own thread with a worker) */
+var UPS = 20; // Game loops per second
+var lastTime = Date.now();
+var gameloopThread = new Worker("/js/loopWorker.js");
+gameloopThread.postMessage([UPS]);
+gameloopThread.onmessage = function(data) {
+    var nowTime = Date.now();
+    update((nowTime - lastTime) / 1000.0);
+    lastTime = nowTime;
+
+    var peopleParagraph = "<ul>";
+    for (var i = 0; i < people.length; i++) {
+        peopleParagraph += "<li>" + people[i].name + ":<br>&nbsp&nbspAge: " + people[i].age.toFixed(1) + "</li>";
+    }
+    peopleParagraph += "</ul>";
+
+    var resourceParagraph = "<p><ul><li>Food: " + resources.food + "</li><li>Build: " + resources.build + "</li></ul></p>";
+
+    var historyParagraph = "<ul>";
+    for (var i = 0; i < historyEntries.length; i++) {
+        historyParagraph += "<li>" + historyEntries[i] + "</li>";
+    }
+    historyParagraph += "</ul>";
+
+    document.getElementById("peopleSheet").innerHTML = peopleParagraph;
+    document.getElementById("resources").innerHTML = resourceParagraph;
+    document.getElementById("history").innerHTML = historyParagraph;
+    gameloopThread.postMessage([UPS]);
+}
